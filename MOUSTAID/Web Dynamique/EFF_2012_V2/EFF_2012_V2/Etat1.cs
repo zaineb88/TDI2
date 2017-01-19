ab93b5a801351bace6ca8bfcc1ede5086e079952
ZAINEB MAZOUZ <z.m.malak@hotmail.com>
2017-01-20 00:15:29 +0100

    modules de pr Moustaid

---
+++ MOUSTAID/Web Dynamique/EFF_2012_V2/EFF_2012_V2/Etat1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EFF_2012_V2
+{
+    public partial class Etat1 : Form
+    {
+        public Etat1()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+            CrystalReport1 CR = new CrystalReport1();
+            crystalReportViewer1.ReportSource = CR;
+        }
+    }
+}
---
+++ MOUSTAID/Web Dynamique/EFF_2012_V2/EFF_2012_V2/Etat2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EFF_2012_V2
+{
+    public partial class Etat2 : Form
+    {
+        public Etat2()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            CrystalReport2 CR = new CrystalReport2();
+            crystalReportViewer1.ReportSource = CR;
+        }
+    }
+}
